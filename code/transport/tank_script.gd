21ade884af03a3d0ab8a4ab5017e4fe2c7f447b6
fst2000 <fst.18.work@gmail.com>
2024-01-27 20:39:08 +0400

    wheel damping

--- code/transport/tank_script.gd
+++ code/transport/tank_script.gd
@@ -1,12 +1,15 @@
-extends Node
+extends RigidBody3D
 
 @export var wheel_bones : Array[Node3D]
 var wheels : Array[Wheel]
 
 func _ready():
 	for b in wheel_bones:
-		wheels.append(Wheel.new(self, b, b.get_node("RayCast3D")))
+		wheels.append(Wheel.new(self, b, b.get_node("RayCast3D"), 5))
 
 func _physics_process(delta):
 	for wheel in wheels:
 		wheel.update(delta)
+		
+	if Input.is_action_just_pressed("jump"):
+		apply_force(Vector3(0,1000,0) * mass)
--- code/transport/wheel.gd
+++ code/transport/wheel.gd
@@ -3,22 +3,31 @@ class_name Wheel
 var transport : RigidBody3D
 var wheel
 var ray : RayCast3D
+var damping_scale
 var wheel_start_position
 var wheel_radius
-func _init(transport, wheel, ray):
+func _init(transport, wheel, ray, damping_scale):
 	self.transport = transport
 	self.wheel = wheel
 	self.ray = ray
+	self.damping_scale = damping_scale
 	wheel_start_position = wheel.position
 	wheel_radius = ray.target_position.length()
 
 func update(delta):
+	damping(delta)
+
+func damping(delta):
 	if ray.is_colliding():
 		var point = ray.get_collision_point()
 		var normal = ray.get_collision_normal()
 		var force_position = point - transport.global_position
 		var torque_quaternion = Quaternion.from_euler(transport.angular_velocity)
 		var velocity_at_point = transport.linear_velocity + (torque_quaternion * force_position - force_position) 
-		var normal_dot = normal.dot(velocity_at_point)
-		var force_scale = transport.mass * -normal_dot * 2
-		transport.apply_force(normal * force_scale, force_position)
+		var velocity_normal_dot = normal.dot(velocity_at_point)
+		var dot_force = max(-velocity_normal_dot, 0) + 0.5
+		var wheel_global_position = transport.global_position + transport.quaternion * wheel_start_position
+		var wheel_damp = (wheel_radius - (wheel_global_position - point).length()) / wheel_radius
+		var force_scale = transport.mass * dot_force * damping_scale * wheel_damp
+		var damping_force = normal * force_scale
+		transport.apply_force(damping_force, force_position)
